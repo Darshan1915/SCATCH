55f6ac1fbcbbc1e9aba36fa70aca51fc96299702
Darshan Bankar <darshanbankar1915@gmail.com>
2024-08-02 23:07:00 +0530

    2 august

--- config/database.js
+++ config/database.js
@@ -1,8 +1,9 @@
 const mongoose = require('mongoose');
+const config = require('config');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/SCATCH")
+        await mongoose.connect(`${config.get("MONGODB_URI")}/SCATCH`)
         console.log("DB connected");
     } catch (error) {
         console.error("DB connection error:", error);
